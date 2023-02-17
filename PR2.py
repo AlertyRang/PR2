d9296208671404c0e3da959d2be7a21379c9224e
AlertyRang <alertyrang@mail.ru>
2023-02-17 12:56:26 +0300

    Project2

--- PR2.py
+++ PR2.py
@@ -1,6 +1,6 @@
 year = int(input("Введите год: "))
 
-if(year % 4 == 0 and year % 100 != 0 or year % 400 == 0 and year % 100 != 0):
+if(year % 4 == 0 and year % 100 != 0 or year % 400 == 0 and year % 100 == 0):
     vess = True
 else:
     vess = False
